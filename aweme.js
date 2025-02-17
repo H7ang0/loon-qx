82260c166286c72278b616e7e7b9d24a79d548b0
HyanGu <98449051+H7ang0@users.noreply.github.com>
2025-02-17 15:09:15 +0800

    Update aweme.js

--- aweme.js
+++ aweme.js
@@ -1,19 +1,23 @@
-// 来自 https://github.com/H7ang0/loon-qx/blob/main/Scripts/douyin.js
-const $ = new API("douyin-shortlink");
+// 来自 https://github.com/H7ang0/loon-qx/blob/main/Scripts/aweme.js
+const $ = new API("aweme-clean");
 const url = $request.url;
 
-(async () => {
-    const res = await $.http.get({ url });
-    const location = res.headers['Location']?.[0];
-    if (location) {
-        const itemId = location.match(/video\/(\d+)/)?.[1];
-        if (itemId) {
-            $done({ 
-                status: 302,
-                headers: { 
-                    Location: `https://www.iesdouyin.com/aweme/v1/web/aweme/detail/?aweme_id=${itemId}`
-                }
-            });
-        }
+if (url.includes('/aweme/v1/feed') || url.includes('/aweme/v1/detail')) {
+    const body = JSON.parse($response.body);
+    if (body.aweme_list) {
+        body.aweme_list.forEach(processVideoItem);
+        $done({ body: JSON.stringify(body) });
     }
-})();
+}
+
+function processVideoItem(item) {
+    if (item.video) {
+        // 替换无水印视频地址
+        item.video.play_addr.url_list = item.video.play_addr.url_list.map(url => 
+            url.replace(/\/playwm\//, '/play/')
+              .replace(/(\?|&)watermark=.*?(&|$)/, '')
+        );
+        // 去除商业化水印
+        delete item.video.download_addr;
+    }
+}
